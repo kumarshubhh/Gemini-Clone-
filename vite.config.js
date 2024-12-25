2b83690c97d17b7bd9c358bc30cc967a63e57d49
kumarshubhh <subhanshukumar290@gmail.com>
2024-12-26 02:08:38 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: process.env.PORT || 5173, // Use the PORT environment variable or default to 5173
+    host: '0.0.0.0', // Ensure it binds to all network interfaces
+  },
+},
+
+)
+
+
+
